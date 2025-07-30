f88a1dd8fe42f642cba1c60a9f72584337b5236f
yinming <1090538449@qq.com>
2025-07-30 19:04:51 +0800

    feat: socket 连接传 token的逻辑

--- src/component/UserChat.vue
+++ src/component/UserChat.vue
@@ -9,25 +9,34 @@
       </ul>
     </section>
     <section>
-      <input type="text" v-modal="message" @keyup.enter="sendMsg" placeholder="请输入消息" />
+      <input type="text" v-modal="message" placeholder="请输入消息" @change="changeFn"/><button @click="sendMsg">发送</button>
     </section>
   </div>
 </template>
 <script lang="ts" setup>
   import {ref, onMounted, onUnmounted } from 'vue';
   import socket from '@/utils/socket';
-
+  const token = localStorage.getItem('token') || 'testUser';
   const message =ref('');
   const messages = ref<string[]>([]);
-
+  const changeFn = (e: Event) => {
+    console.log(e)
+    if(e.target && e.target instanceof HTMLInputElement) {
+      message.value = e.target.value;
+    }
+  }
   const sendMsg = () => {
     if(message.value.trim()) {
-      socket.emit('chat message: ', message.value);
+      socket.emit('chat message', message.value);
       message.value = '';
     }
   }
   onMounted(() => {
     // 需要进行通信的时候才创建连接
+    // 携带token
+    socket.auth = {
+      token
+    }
     socket.connect();
     socket.on("connect", () => {
       console.log('connect success');
@@ -35,7 +44,9 @@
     socket.on('disconnect', () => {
       console.log('disconnected');
     })
-
+    socket.on('connect_error', (err: Error) => {
+      console.log('连接失败: ', err.message);
+    })
     socket.on('chat message', (msg: string) => {
       if(!msg.length) {
         return;
