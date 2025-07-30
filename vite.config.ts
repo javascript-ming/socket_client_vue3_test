e1e68dde0a7d5fb4046e9b1c3054b9cb71ed4685
yinming <1090538449@qq.com>
2025-07-30 11:58:55 +0800

    feat: 编译配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import vue from "@vitejs/plugin-vue";
+import vueDevTools from "vite-plugin-vue-devtools";
+// import vueJsx from "@vitejs/plugin-vue-jsx";
+// import { default as vueJsx } from "@vitejs/plugin-vue-jsx";
 import { resolve } from "path";
 import fs from "fs";
 
@@ -14,9 +15,10 @@ export default defineConfig({
       key: fs.readFileSync(resolve(__dirname, "./cert/localhost-key.pem")),
       cert: fs.readFileSync(resolve(__dirname, "./cert/localhost.pem"))
     },
-    port: 6000
+    port: 6000,
+    host: true
   },
-  plugins: [vue(), vueJsx(), vueDevTools()],
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url))
